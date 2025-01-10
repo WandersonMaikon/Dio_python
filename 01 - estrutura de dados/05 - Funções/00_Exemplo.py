f5931134f51de6482092a75cc996e94fb4ed11b4
WandersonMaikon <wandersonmaikon@gmail.com>
2025-01-10 15:38:03 -0400

    Adicionando funções

---
+++ 01 - estrutura de dados/05 - Funções/00_Exemplo.py
@@ -0,0 +1,12 @@
+# Criação de funções
+def exibir_mensagem():
+    print("Olá mundo!")
+def exibir_mensagem_2(nome):
+    print(f"Seja bem vindo {nome}!")
+def exibir_mensagem_3(nome="Anônimo"):
+    print(f"Seja bem vindo {nome}!")
+
+exibir_mensagem()
+exibir_mensagem_2(nome="joão")
+exibir_mensagem_3()
+exibir_mensagem_3(nome="Judite")

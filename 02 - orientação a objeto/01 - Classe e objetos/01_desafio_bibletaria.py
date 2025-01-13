a2bee72ff5c07f0217aa6f4da0953adefd6c3787
WandersonMaikon <wandersonmaikon@gmail.com>
2025-01-13 17:13:46 -0400

    Construtor e herança

--- 02 - orientação a objeto/01 - Classe e objetos/01_desafio_bibletaria.py
+++ 02 - orientação a objeto/01 - Classe e objetos/01_desafio_bibletaria.py
@@ -14,10 +14,13 @@ class Bicicleta:
     def correr(self):
         print("Vrummmmm")
 
+
 b1 = Bicicleta("vermelha", "caloi", 2022, 650)
 b1.buzinar()
 b1.correr()
 b1.parar()
 print(b1.cor,b1.modelo, b1.ano, b1.valor)
 
-b2 = Bicicleta("Vermelho")
+b2 = Bicicleta("Vermelho", "Caloi", 2000, 120)
+b2.buzinar()
+print(b2.cor)
---
+++ 02 - orientação a objeto/02 - construtor/Cachorro.py
@@ -0,0 +1,6 @@
+class Cachrro:
+    def __init__(self, raca, nome, idade):
+        self.raca = raca
+        self.nome = nome
+        self.idade = idade
+    
